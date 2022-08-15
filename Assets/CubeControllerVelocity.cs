0bcdefc9fd9265685c0f3917f90f6c93d9f95df5
Matt Davis <m_davis@outlook.com>
2022-08-15 16:29:51 +1000

    CubeControllerVelocity hints

---
+++ Assets/CubeControllerVelocity.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControllerVelocity : MonoBehaviour
+{
+    [SerializeField] float acceleration;
+
+    Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        // Method 1
+        var movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        // Set velocity based on movement
+        velocity += .....
+
+        // Set position based on velocity
+        transform.position += .....
+    }
+}
