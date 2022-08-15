4d7e52a76c0158eb5dfc5a173399254f638b34e9
Matt Davis <m_davis@outlook.com>
2022-08-15 14:11:26 +1000

    Create CubeA

---
+++ Assets/ZAxisPingPong.cs
@@ -0,0 +1,18 @@
+﻿// COMP30019 - Graphics and Interaction
+// (c) University of Melbourne, 2022
+
+using UnityEngine;
+
+public class ZAxisPingPong : MonoBehaviour
+{
+    [SerializeField] private float positionExtent = 1f;
+    [SerializeField] private float flipPeriod = 1f;
+
+    private void Update()
+    {
+        var normalizedPosition = Mathf.PingPong(Time.time, flipPeriod) / flipPeriod;
+        var pos = Mathf.Lerp(-positionExtent, positionExtent, normalizedPosition) * Vector3.forward;
+
+        transform.localPosition = pos;
+    }
+}
